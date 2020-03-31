cc39720966753af404243006f480b57d032cf7a3
melissagovender9701 <melissa.govender9701@gmail.com>
2020-03-31 13:30:09 +0200

    attempt on graph but failed ...

--- src/app/Components/country/country.component.ts
+++ src/app/Components/country/country.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CoronaService } from 'src/app/Services/corona.service';
-
+import { Chart } from 'chart.js';
 @Component({
   selector: 'app-country',
   templateUrl: './country.component.html',
@@ -8,7 +8,7 @@ import { CoronaService } from 'src/app/Services/corona.service';
 })
 export class CountryComponent implements OnInit {
   info: any = null;
-
+  blah: any = null;
   ngOnInit(): void {
     
   }
@@ -16,15 +16,19 @@ export class CountryComponent implements OnInit {
     this.coronaService.getCountryData().subscribe((data)=>{
       this.info = data;
     });
-    this.getGraphStats();
+  this.getGraphStats();
   }
   getGraphStats(){
-    fetch("https://pomber.github.io/covid19/timeseries.json")
-  .then(response => response.json())
-  .then(data => {
+    this.coronaService.getStats().subscribe((data:any)=>{
+      this.blah = data;
     data["South Africa"].forEach(({ date, confirmed, recovered, deaths }) =>
       console.log(`${date} active cases: ${confirmed - recovered - deaths}`)
+      
     );
-  });
+
+
+      
+    })
+    
   }
 }
--- src/app/Services/corona.service.ts
+++ src/app/Services/corona.service.ts
@@ -6,11 +6,13 @@ import { HttpClient } from '@angular/common/http';
 export class CoronaService {
 
   apiUrl = 'https://coronavirus-19-api.herokuapp.com/countries/south%20africa';
-
+  statsUrl = 'https://pomber.github.io/covid19/timeseries.json';
   constructor(private http: HttpClient) { }
 
   getCountryData(){
     return this.http.get(`${this.apiUrl}`);
   }
-
+  getStats(){
+    return this.http.get(`${this.statsUrl}`);
+  }
 }
