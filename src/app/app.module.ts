5f5b713f8d0fceab00bb391e792116016c3b8921
melissagovender9701 <melissa.govender9701@gmail.com>
2020-03-31 16:21:32 +0200

    safety, videos and home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavComponent } from './Components/top-nav/top-nav.component';
 import { CountryComponent } from './Components/country/country.component';
+import { HomeComponent } from './Components/home/home.component';
+import { SafetyComponent } from './Components/safety/safety.component';
+import { VideosComponent } from './Components/videos/videos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavComponent,
-    CountryComponent
+    CountryComponent,
+    HomeComponent,
+    SafetyComponent,
+    VideosComponent
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
